576faf1849854b98f3a2483a4b8a93157a9beb95
Heitor Nobuo <heitor.nozu@ebanx.com>
2022-12-01 19:14:21 -0300

    change proto path

--- grpc-node-consumer/index.js
+++ grpc-node-consumer/index.js
@@ -1,7 +1,10 @@
+
 const grpc = require('@grpc/grpc-js');
+
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path')
-const protoObject = protoLoader.loadSync(path.resolve(__dirname+'/protos/Message.proto'));
+
+const protoObject = protoLoader.loadSync(path.resolve('../proto/Message.proto'));
 const package = grpc.loadPackageDefinition(protoObject);
 const client = new package.com.heitor.grpc.MessageService('localhost:50051', grpc.credentials.createInsecure())
 setInterval(() => {
